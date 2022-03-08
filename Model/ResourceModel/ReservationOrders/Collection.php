d3dc9a5e6819a17cceea389183c8fd15da4a1873
Alin Balan <balanalin2003@gmail.com>
2022-03-08 14:01:48 -0500

    fix collection reservation orders issue

--- Model/ResourceModel/ReservationOrders/Collection.php
+++ Model/ResourceModel/ReservationOrders/Collection.php
@@ -3,6 +3,7 @@
 namespace SalesIgniter\Rental\Model\ResourceModel\ReservationOrders;
 
 //use \Magento\Eav\Api\AttributeRepositoryInterface;
+use Magento\Eav\Model\ResourceModel\Entity\Attribute;
 
 class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection
 {
@@ -10,6 +11,41 @@ class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\Ab
      * @var string
      */
     protected $_idFieldName = 'reservationorder_id';
+
+    /**
+     * @var Attribute
+     */
+    private $eavAttribute;
+
+
+    /**
+     * @param \Magento\Framework\Data\Collection\EntityFactoryInterface $entityFactory
+     * @param \Psr\Log\LoggerInterface $logger
+     * @param \Magento\Framework\Data\Collection\Db\FetchStrategyInterface $fetchStrategy
+     * @param \Magento\Framework\Event\ManagerInterface $eventManager
+     * @param Attribute $eavAttribute
+     * @param \Magento\Framework\DB\Adapter\AdapterInterface $connection
+     * @param \Magento\Framework\Model\ResourceModel\Db\AbstractDb $resource
+     */
+    public function __construct(
+        \Magento\Framework\Data\Collection\EntityFactoryInterface $entityFactory,
+        \Psr\Log\LoggerInterface $logger,
+        \Magento\Framework\Data\Collection\Db\FetchStrategyInterface $fetchStrategy,
+        \Magento\Framework\Event\ManagerInterface $eventManager,
+        Attribute $eavAttribute,
+        \Magento\Framework\DB\Adapter\AdapterInterface $connection = null,
+        \Magento\Framework\Model\ResourceModel\Db\AbstractDb $resource = null
+    ) {
+        $this->eavAttribute = $eavAttribute;
+        parent::__construct(
+            $entityFactory,
+            $logger,
+            $fetchStrategy,
+            $eventManager,
+            $connection,
+            $resource
+        );
+    }
 
     /**
      * Constructor
@@ -26,30 +62,38 @@ class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\Ab
     protected function _initSelect()
     {
         parent::_initSelect();
-        $filterSubSelect = $this->getSelect()->getConnection()->select();
-        $filterSubSelect->from(
-            ['cpev' => $this->getTable('catalog_product_entity_varchar')],
-            ['value as name']
-        )->joinLeft(
-            ['ea' => $this->getTable('eav_attribute')],
-            'cpev.attribute_id = ea.attribute_id',
-            []
-        )->where(
-            'ea.attribute_code = ?', 'name'
-        )->where(
-            'cpev.entity_id = main_table.product_id'
-        )->limit(1);
+       //$filterSubSelect = $this->getSelect()->getConnection()->select();
+       //$filterSubSelect->from(
+       //    ['cpev' => $this->getTable('catalog_product_entity_varchar')],
+       //    ['value as name']
+       //)->joinLeft(
+       //    ['ea' => $this->getTable('eav_attribute')],
+       //    'cpev.attribute_id = ea.attribute_id',
+       //    []
+       //)->where(
+       //    'ea.attribute_code = ?', 'name'
+       //)->where(
+       //    'cpev.entity_id = main_table.product_id'
+       //)->limit(1);
+
+        $attributeId = $this->eavAttribute->getIdByCode('catalog_product', 'name');
 
         //$columnExpression = \Magento\Framework\App\ObjectManager::getInstance()->get('Magento\Framework\DB\Sql\ColumnValueExpressionFactory')->create([
         //  'expression' => '('.$filterSubSelect->__toString().')',
         //]);
-        $this->getSelect()->columns(['name' => '('.$filterSubSelect->__toString().')']);
+        //$this->getSelect()->columns(['name' => '('.$filterSubSelect->__toString().')']);
         $this->getSelect()->joinLeft(
             ['ot' => $this->getTable('sales_order')],
             'main_table.order_id = ot.entity_id',
             ['increment_id']
         );
-
+        $this->getSelect()->joinLeft(
+            ['cpev' => $this->getTable('catalog_product_entity_varchar')],
+            'cpev.entity_id = main_table.product_id',
+            ['name' => 'cpev.value']
+        );
+        $this->getSelect()->where('cpev.attribute_id = ?', $attributeId);
+        $this->getSelect()->where('cpev.store_id = 0');
         return $this;
     }
 
